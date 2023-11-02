d611488b4850bb3307bfe006d0a10c4e77af7460
PhilKubsz <philip.kubisz@gmail.com>
2023-11-02 14:10:23 -0700

    Frontend: Fully responsive adding collapsing sidebar option for mobile screensizes

--- mern-chat-app/src/App.js
+++ mern-chat-app/src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Chat from './Components/Chat/Chat';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(window.innerWidth > 768);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSidebarOpen(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="app">
-      <div className="appBody">
-      <Sidebar />
-      <Chat />
+      <div className={`appBody ${isSidebarOpen ? '' : 'collapsed'}`}>
+        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <Chat />
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
--- mern-chat-app/src/Components/Sidebar/Sidebar.jsx
+++ mern-chat-app/src/Components/Sidebar/Sidebar.jsx
@@ -1,47 +1,56 @@
 import React from 'react';
 import './Sidebar.css';
-import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import SidebarChat from '../SidebarChat/SidebarChat';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
-
-function Sidebar() {
+function Sidebar({ isSidebarOpen, toggleSidebar }) {
   return (
-    <div className='sidebar'>
-      <div className='sidebarHeader'>
-        <Avatar src="https://avatars.githubusercontent.com/u/122698773?v=4"/>
-        <div className='sidebarHeaderRight'>
-        </div>
-          <IconButton>
-            <DonutLargeIcon />
-          </IconButton>
-          <IconButton>
-            <ChatIcon />
-          </IconButton>
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
+    <div className={isSidebarOpen ? 'sidebar' : 'sidebar collapsed'}>
+      <div className="toggleButtonContainer">
+        <IconButton className="toggleButton" onClick={toggleSidebar}>
+          <ArrowBackIosNewIcon />
+        </IconButton>
       </div>
-      <div className='sidebarSearch'>
-        <div className='sidebarSearchContainer'>
-          <SearchOutlinedIcon />
-          <input placeholder='Search or start a new chat' type='text' />
-        </div>
-      </div>
-
-      <div className='sidebarChats'>
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-      </div>
-
-
+      {isSidebarOpen && (
+        <>
+          <div className="sidebarContent">
+            <div className="sidebarHeader">
+              <Avatar src="https://avatars.githubusercontent.com/u/122698773?v=4" />
+              <div className="sidebarHeaderRight">
+                <IconButton>
+                  <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                  <ChatIcon />
+                </IconButton>
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              </div>
+            </div>
+            <div className="sidebarSearch">
+              <div className="sidebarSearchContainer">
+                <SearchOutlinedIcon />
+                <input placeholder="Search or start a new chat" type="text" />
+              </div>
+            </div>
+            <div className="sidebarChats">
+              <SidebarChat />
+              <SidebarChat />
+              <SidebarChat />
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
+
